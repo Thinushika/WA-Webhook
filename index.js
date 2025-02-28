556983bea42e3646870a6b581a3660e9be73d7ac
tj <thinupavani@gmail.com>
2025-02-28 12:52:18 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,76 @@
+const express = require("express");
+const axios = require("axios");
+require('dotenv').config()  
+
+const app = express();
+app.use(express.json());
+
+const { WEBHOOK_VERIFY_TOKEN, GRAPH_API_TOKEN, PORT } = process.env;
+
+app.post("/webhook", async (req, res) => {
+
+  console.log("Incoming webhook message:", JSON.stringify(req.body, null, 2));
+  
+  const message = req.body.entry?.[0]?.changes[0]?.value?.messages?.[0];
+
+  if (message?.type === "text") {
+    const business_phone_number_id =
+      req.body.entry?.[0].changes?.[0].value?.metadata?.phone_number_id;
+
+    await axios({
+      method: "POST",
+      url: `https://graph.facebook.com/v18.0/${business_phone_number_id}/messages`,
+      headers: {
+        Authorization: `Bearer ${GRAPH_API_TOKEN}`,
+      },
+      data: {
+        messaging_product: "whatsapp",
+        to: message.from,
+        text: { body: "Hi.. I'm TJ" },
+        context: {
+          message_id: message.id, 
+        },
+      },
+    });
+
+    await axios({
+      method: "POST",
+      url: `https://graph.facebook.com/v18.0/${business_phone_number_id}/messages`,
+      headers: {
+        Authorization: `Bearer ${GRAPH_API_TOKEN}`,
+        "Content-Type": 'application/json'
+      },
+      data: {
+        messaging_product: "whatsapp",
+        status: "read",
+        message_id: message.id,
+      },
+    });
+  }
+
+  res.sendStatus(200);
+});
+
+app.get("/webhook", (req, res) => {
+  const mode = req.query["hub.mode"];
+  const token = req.query["hub.verify_token"];
+  const challenge = req.query["hub.challenge"];
+
+  if (mode === "subscribe" && token === WEBHOOK_VERIFY_TOKEN) {
+    res.status(200).send(challenge);
+    console.log("Webhook verified successfully!");
+  } else {
+    res.sendStatus(403);
+  }
+});
+
+app.get("/", (req, res) => {
+  console.log("hi")
+  res.send(`<pre>Nothing to see here.
+Checkout README.md to start.</pre>`);
+});
+
+app.listen(PORT, () => {
+  console.log("hi")
+  console.log(`Server is a listening on port: ${PORT}`);
+});
